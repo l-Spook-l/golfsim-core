bb003d3eca707eeefcf81a6fdb38d9ce0c46a2da
l-Spook-l <uaspookua@gmail.com>
2024-02-10 21:01:24 +0200

    create model for shot

---
+++ Data_Base/models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from .config_db import Base
+
+
+class UserSettings(Base):
+    __tablename__ = 'user_settings_table'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    speed: Mapped[float]
+    angle: Mapped[float]
+    distance: Mapped[int]
+    date: Mapped[datetime] = mapped_column(default=datetime.utcnow())
+
+    # id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    # user: Mapped[int]
+    # DK021_2015: Mapped[str]
+    # Status: Mapped[str]
+    # Procurement_type: Mapped[str]
+    # Region: Mapped[str]
+    # Dispatch_time: Mapped[str]
+    # Email: Mapped[str]
+    # created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow())
