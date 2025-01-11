42cbcb25553833bf731594a790282f9bb92d424b
l-Spook-l <uaspookua@gmail.com>
2025-01-11 23:13:28 +0200

    Temporary test data

---
+++ gui/test_data_club.py
@@ -0,0 +1,191 @@
+# мили ярды
+data_club = {
+    "golf_hits": [
+        {
+            "club": "Driver",
+            "distance": 253,
+            "ball_speed": 107,
+            "date": "2024-01-01"
+        },
+        {
+            "club": "3-Wood",
+            "distance": 217,
+            "ball_speed": 91,
+            "date": "2024-01-02"
+        },
+        {
+            "club": "5-Wood",
+            "distance": 188,
+            "ball_speed": 83,
+            "date": "2024-01-03"
+        },
+        {
+            "club": "7-Wood",
+            "distance": 176,
+            "ball_speed": 88,
+            "date": "2024-01-04"
+        },
+        {
+            "club": "9-Wood",
+            "distance": 166,
+            "ball_speed": 77,
+            "date": "2024-01-05"
+        },
+        {
+            "club": "3-Iron",
+            "distance": 180,
+            "ball_speed": 77,
+            "date": "2024-01-06"
+        },
+        {
+            "club": "4-Iron",
+            "distance": 174,
+            "ball_speed": 78,
+            "date": "2024-01-07"
+        },
+        {
+            "club": "5-Iron",
+            "distance": 158,
+            "ball_speed": 72,
+            "date": "2024-01-08"
+        },
+        {
+            "club": "6-Iron",
+            "distance": 153,
+            "ball_speed": 66,
+            "date": "2024-01-09"
+        },
+        {
+            "club": "7-Iron",
+            "distance": 146,
+            "ball_speed": 58,
+            "date": "2024-01-10"
+        },
+        {
+            "club": "8-Iron",
+            "distance": 132,
+            "ball_speed": 58,
+            "date": "2024-01-11"
+        },
+        {
+            "club": "9-Iron",
+            "distance": 122,
+            "ball_speed": 56,
+            "date": "2024-01-12"
+        },
+        {
+            "club": "Pitching Wedge",
+            "distance": 111,
+            "ball_speed": 48,
+            "date": "2024-01-13"
+        },
+        {
+            "club": "Gap Wedge",
+            "distance": 102,
+            "ball_speed": 47,
+            "date": "2024-01-14"
+        },
+        {
+            "club": "Sand Wedge",
+            "distance": 93,
+            "ball_speed": 36,
+            "date": "2024-01-15"
+        },
+        {
+            "club": "Lob Wedge",
+            "distance": 82,
+            "ball_speed": 32,
+            "date": "2024-01-16"
+        },
+        # {
+        #     "club": "Putter",
+        #     "distance": 20,
+        #     "ball_speed": 5,
+        #     "date": "2024-01-17"
+        # },
+        {
+            "club": "Hybrid 3",
+            "distance": 207,
+            "ball_speed": 102,
+            "date": "2024-01-18"
+        },
+        {
+            "club": "Hybrid 4",
+            "distance": 186,
+            "ball_speed": 96,
+            "date": "2024-01-19"
+        },
+        {
+            "club": "Hybrid 5",
+            "distance": 161,
+            "ball_speed": 87,
+            "date": "2024-01-20"
+        },
+        {
+            "club": "Utility Club",
+            "distance": 178,
+            "ball_speed": 92,
+            "date": "2024-01-21"
+        },
+        {
+            "club": "2-Iron",
+            "distance": 192,
+            "ball_speed": 98,
+            "date": "2024-01-22"
+        },
+        {
+            "club": "3-Iron",
+            "distance": 182,
+            "ball_speed": 93,
+            "date": "2024-01-23"
+        },
+        {
+            "club": "4-Iron",
+            "distance": 170,
+            "ball_speed": 88,
+            "date": "2024-01-24"
+        },
+        {
+            "club": "5-Iron",
+            "distance": 160,
+            "ball_speed": 87,
+            "date": "2024-01-25"
+        },
+        {
+            "club": "6-Iron",
+            "distance": 155,
+            "ball_speed": 73,
+            "date": "2024-01-26"
+        },
+        {
+            "club": "7-Iron",
+            "distance": 142,
+            "ball_speed": 76,
+            "date": "2024-01-27"
+        },
+        {
+            "club": "8-Iron",
+            "distance": 130,
+            "ball_speed": 65,
+            "date": "2024-01-28"
+        },
+        {
+            "club": "9-Iron",
+            "distance": 120,
+            "ball_speed": 60,
+            "date": "2024-01-29"
+        },
+        {
+            "club": "Pitching Wedge",
+            "distance": 110,
+            "ball_speed": 55,
+            "date": "2024-01-30"
+        },
+        {
+            "club": "Gap Wedge",
+            "distance": 100,
+            "ball_speed": 50,
+            "date": "2024-01-31"
+        }
+    ]
+}
