c57f993c0189e25ecc794ae26ea9d1343ee3562c
l-Spook-l <uaspookua@gmail.com>
2025-02-22 21:09:12 +0200

    Added a method to retrieve data about the last shot.

--- Data_Base/db.py
+++ Data_Base/db.py
@@ -1,4 +1,6 @@
 from sqlalchemy import insert, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from .models import GolfShot
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -15,6 +17,17 @@ class DataBase:
         except Exception as error:
             print(f"Error occurred while adding data: {error}")
             return False
+
+    @classmethod
+    async def get_last_shot(cls, session: AsyncSession):
+        query = (
+            select(GolfShot.carry, GolfShot.ball_speed, GolfShot.angle_v, GolfShot.angle_h)
+            .order_by(GolfShot.date.desc())
+            .limit(1)
+        )
+        result = await session.execute(query)
+        last_shot = tuple(str(item) for item in result.fetchone())
+        return last_shot  # Вернем объект GolfShot
 
     @classmethod
     async def get_data(cls, session: AsyncSession):
