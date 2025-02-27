3371c1a0a0e2cc053ca42c184ad99e1e3d9dec2b
l-Spook-l <uaspookua@gmail.com>
2025-02-27 22:28:36 +0200

    Implemented local IP address discovery

---
+++ web_server/find_local_ip.py
@@ -0,0 +1,17 @@
+import socket
+
+
+def get_local_ip() -> str:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # Подключаемся к публичному DNS серверу Google
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+    except Exception as e:
+        local_ip = "127.0.0.1"
+    finally:
+        s.close()
+    return local_ip
+
+
+print(f"Local IP address: {get_local_ip()}")
