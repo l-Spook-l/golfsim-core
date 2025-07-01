eae2e91bc2f881e33f02f60a1719a5968520fda5
l-Spook-l <uaspookua@gmail.com>
2025-07-01 22:05:06 +0300

    Improved table structure for drive range

--- gui/tab_stat_table.py
+++ gui/tab_stat_table.py
@@ -40,40 +40,57 @@ async def load_stat_tab():
     data = await load_data()
 
     # Создание таблицы
+    headers = (
+        "N", "Club", "Ball\n(mph)", "Launch V\n(deg)", "Launch H\n(deg)", "Carry\n(yd)",
+        "Roll\n(yd)", "Total\n(yd)", "Lateral\n(yd)", "Spin\n(rpm)", "Date"
+    )
 
     table_header = ft.Row([
         ft.Container(
-            content=ft.Text(header, size=19, text_align=ft.TextAlign.CENTER),
-            # alignment=ft.alignment.center,
-            width=95,
-            bgcolor="orange"
+            content=ft.Text(header, size=18, weight=ft.FontWeight.BOLD, text_align=ft.TextAlign.CENTER),
+            width=171,
+            bgcolor="#C8E6C9",
+            border=ft.border.all(1, "#BDBDBD"),
+            border_radius=ft.border_radius.only(top_left=6, top_right=6) if i == 0 else None,
+            padding=0,
+            margin=0,
+            alignment=ft.alignment.center
         )
-        for header in headers
-    ])
+        for i, header in enumerate(headers)
+    ], spacing=0, height=70)
 
-    table_body = ft.Column([
-        ft.Row(
-            [
+    # Строки таблицы
+    table_body = ft.Column(
+        controls=[
+            ft.Row([
                 ft.Container(
-                    content=ft.Text(cell, size=18, text_align=ft.TextAlign.CENTER),
-                    # alignment=ft.alignment.center,
-                    width=95
+                    content=ft.Text(cell, size=16, text_align=ft.TextAlign.CENTER, color="#212121"),
+                    width=171,
+                    bgcolor="#E8F5E9" if idx % 2 == 0 else "#C8E6C9",  # Зебра
+                    border=ft.border.all(1, "#BDBDBD"),
+                    # padding=6,
+                    alignment=ft.alignment.center,
                 )
                 for cell in row
-            ]
-        )
-        for row in data
-    ])
+            ], spacing=0, height=70)
+            for idx, row in enumerate(data)
+        ],
+        spacing=0,
+        height=430,
+        scroll=ft.ScrollMode.ALWAYS
+    )
 
     return ft.Container(
         content=ft.Column([
             table_header,
-            ft.Column(
-                controls=[table_body],
-                height=450,
-                scroll=ft.ScrollMode.ALWAYS  # заставляет колонку прокручиваться
-            )
-        ]),
-        border=ft.border.all(1, "grey"),
+            ft.Divider(
+                thickness=3,  # Толщина линии
+                color="grey",  # Цвет линии
+                height=0
+            ),
+            table_body
+        ], spacing=0
+        ),
+        border=ft.border.all(1, "#BDBDBD"),
         border_radius=10,
     )
