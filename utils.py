bacbc66b15c2728d439fe1e2c3951803a164fd5d
l-Spook-l <uaspookua@gmail.com>
2025-07-28 23:25:07 +0300

    feat: add utils file

---
+++ utils.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+import aiofiles
+from logging_config import logger
+
+
+async def load_settings() -> dict:
+    data = {}  # значение по умолчанию
+
+    # 1. Прочитать файл, если он существует
+    if os.path.exists("settings.json"):
+        async with aiofiles.open("settings.json", "r", encoding="utf-8") as f:
+            try:
+                content = await f.read()
+                if content.strip():  # Проверка на пустоту
+                    data = json.loads(content)
+                else:
+                    logger.info("Файл пустой. Создаём заново.")
+            except json.JSONDecodeError:
+                logger.info("Файл повреждён. Создаём заново.")
+
+    return data
