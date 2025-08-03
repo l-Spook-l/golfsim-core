98599469194c2d0056014aee97608671757fd5b1
l-Spook-l <uaspookua@gmail.com>
2025-08-03 21:04:32 +0300

    feat: implemented table update on date filter apply

--- gui/drive_range_dashboard.py
+++ gui/drive_range_dashboard.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 import flet as ft
 
-from gui.tab_stat_table import load_stat_tab
+from gui.filter_bar import FilterBar
+from gui.tab_stat_table import GolfShotTable
 # from gui.tab_stat_graph import load_stat_graph
 from data_base.config_db import async_session_maker
 from data_base.repositories.golf_shot import GolfShotRepository
@@ -14,10 +15,6 @@ json_path = os.path.join(os.path.dirname(__file__), '../data/clubs.json')
 
 # with open(json_path, 'r', encoding='utf-8') as file:
 #     clubs_data = json.load(file)
-
-golf_list_clubs = ("All clubs", "Driver", "3-Wood", "5-Wood", "4-Iron", "5-Iron", "6-Iron", "7-Iron", "8-Iron",
-                   "9-Iron", "Pitching Wedge", "Gap Wedge", "Sand wedge", "Lob wedge", "Putter")
-
 
 # unit_system = {
 #     "Imperial": {"Distance": "Yards", "Speed": "mph"},
@@ -29,46 +26,52 @@ golf_list_clubs = ("All clubs", "Driver", "3-Wood", "5-Wood", "4-Iron", "5-Iron"
 class DriveRangeDashboard:
     def __init__(self):
         self.page = AppContext.get_page()
+        self.load_table = GolfShotTable()
+        self.load_filter_bar = FilterBar(dashboard=self)
         self.table = ft.Container()
-        self.tabs = ft.Tabs()
-        self.dashboard_content = ft.Column()
+        self.filter_bar = ft.Container()
+        self.view_selector = ft.Tabs()
+        self.dashboard_content = ft.Container()
+        self.tab_content = ft.Column()
 
-    async def change_view_graph(self, e):
-        # nonlocal date_filter_block  # Позволяет менять переменную в родительской области
+        self.golf_list_clubs = (
+            "All clubs", "Driver", "3-Wood", "5-Wood", "4-Iron", "5-Iron", "6-Iron", "7-Iron", "8-Iron",
+            "9-Iron", "Pitching Wedge", "Gap Wedge", "Sand wedge", "Lob wedge", "Putter")
 
+        self.chart = ft.Text("Test tab")
+
+    async def change_view_dashboard(self, e):
         # Определяем текущий активный контент (график или таблица)
         current_tab = None
 
-        match self.tabs.selected_index:
+        match self.view_selector.selected_index:
             case 0:
                 current_tab = self.table
-            # case 1:
-            #     current_tab = chart
+            case 1:
+                current_tab = self.chart
 
-        # chart_with_padding.content = current_tab  # Меняем контент
+        self.dashboard_content.content = current_tab
+        self.dashboard_content.update()
+        self.tab_content.update()
 
-        # Пересоздаем фильтр, передавая текущий активный элемент
-        # date_filter_block.content = await create_filter_bar(
-        #     # current_tab, tabs.selected_index, page, dropdown_select_club, dropdown_select_unit_system
-        #     current_tab, tabs.selected_index, page, dropdown_select_club, dropdown_sort
-        # )
-
-        # date_filter_block.update()
+    async def update(self, start_date, end_date):
+        data = await GolfShotTable().load_data(start_date, end_date)
+        self.table = await self.load_table.load_stat_table(data)
+        self.dashboard_content.content = self.table
         self.dashboard_content.update()
 
     async def build_section(self):
         # Загружаем данные
-        self.table = await load_stat_tab()
+        self.table = await self.load_table.load_stat_table()
+        self.filter_bar = await self.load_filter_bar.build_section()
 
-        # chart = await load_stat_graph()
-        chart = ft.Text("sadddddddddddd")
-
-        tabs = ft.Tabs(
+        # панель переключения графиков
+        self.view_selector = ft.Tabs(
             selected_index=0,
-            on_change=self.change_view_graph,
+            on_change=self.change_view_dashboard,
             tabs=[
                 ft.Tab(text="Table"),
-                # ft.Tab(text="Graph"),  # график временно убран
+                ft.Tab(text="Graph"),  # график временно убран
             ],
             label_color="black"
         )
@@ -95,29 +98,22 @@ class DriveRangeDashboard:
         #     # page.update()
 
         # Оборачиваем график в контейнер для отступов
-        chart_with_padding = ft.Container(
+        self.dashboard_content = ft.Container(
             content=self.table,
             # margin=ft.margin.all(50),  # Задаем отступы со всех сторон
             # bgcolor=ft.Colors.with_opacity(0.1, ft.Colors.BLUE),  # Фоновый цвет для визуализации
         )
 
-        # # Создаем элементы UI
-        # # dropdown_select_club, dropdown_select_unit_system = create_dropdowns(chart, page)
-        # dropdown_select_club, dropdown_sort = create_dropdown(chart, page)
-        #
-        # date_filter_block = await create_filter_bar(
-        #     # table, tabs.selected_index, page, dropdown_select_club, dropdown_select_unit_system
-        #     table, tabs.selected_index, page, dropdown_select_club, dropdown_sort
-        # )
-
-        dashboard_content = ft.Column(
+        self.tab_content = ft.Column(
             [
                 # date_filter_block,
-                tabs,
+                self.filter_bar,
+                self.view_selector,
                 # ft.Row([dropdown_select_club, dropdown_select_unit_system], spacing=30),
-                chart_with_padding
+                self.dashboard_content
             ],
             # scroll=ft.ScrollMode.ALWAYS,
         )
 
-        return dashboard_content
+        return self.tab_content
+
