85ff4a894898d9f84f820739e48db2f87d85d55f
l-Spook-l <uaspookua@gmail.com>
2025-08-07 23:34:40 +0300

    style: adjusted width of some UI components

--- gui/tab_stat_table.py
+++ gui/tab_stat_table.py
@@ -41,7 +41,7 @@ class GolfShotTable:
         table_header = ft.Row([
             ft.Container(
                 content=ft.Text(header, size=18, weight=ft.FontWeight.BOLD, text_align=ft.TextAlign.CENTER),
-                width=171,
+                width=174,
                 bgcolor="#C8E6C9",
                 border=ft.border.all(1, "#BDBDBD"),
                 border_radius=ft.border_radius.only(top_left=6, top_right=6) if i == 0 else None,
@@ -58,13 +58,13 @@ class GolfShotTable:
                 ft.Row([
                     ft.Container(
                         content=ft.Text(cell, size=16, text_align=ft.TextAlign.CENTER, color="#212121"),
-                        width=171,
+                        width=174,
                         bgcolor="#E8F5E9" if idx % 2 == 0 else "#C8E6C9",  # Зебра
                         border=ft.border.all(1, "#BDBDBD"),
                         alignment=ft.alignment.center,
                     )
                     for cell in row
-                ], spacing=0, height=45)
+                ], spacing=0, height=48)
                 for idx, row in enumerate(data)
             ],
             spacing=0,
