13af2ced7da53aa41fa1693073322cdac1b56b3b
l-Spook-l <uaspookua@gmail.com>
2025-08-11 19:40:59 +0300

    refactor: Update database configuration for async SQLite with SQLAlchemy

--- Data_Base/config_db.py
+++ Data_Base/config_db.py
@@ -1,6 +1,5 @@
-from typing import AsyncGenerator
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 
 DATABASE_URL = "sqlite+aiosqlite:///example.db"
@@ -12,9 +11,3 @@ class Base(DeclarativeBase):
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
-        yield session
-
